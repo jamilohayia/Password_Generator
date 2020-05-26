1f1b4370af3bf830a3e6e98a2a0e9a9c031fc733
Jamil Ohayia <jamil.ohayia@columbia.edu>
2020-05-25 23:08:23 -0400

    this is the second push

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,73 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
-
-
 function writePassword() {
-  var length = prompt("Please enter a password");
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-  function generatePassword () {
-
-    for.generatePassword
-    var UpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    
- }
+  var length = prompt("Passwords lengths must be between 8 and 128 characters.  How many characters would you like for your password to be?");
+  while (length < 8 || length > 128) { 
+  var length 
+  }
 
 }
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
+generateBtn.addEventListener("click", function() {
+if (generateBtn) {
+  
+  if length = prompt("Please choose a number ")
+}
+
+
+
+// Prompts for the user input.  
+  function generatePassword () { 
+  var UserUpper = confirm("Would you like to include uppercase letters?")
+  var UserLower = confirm("Would you like to include lowercase letters?")
+  var UserNumbers = confirm("Would you like to include numbers?")
+  var UserSpecial = confirm("Would you like to include special characters?")
+
+}
+
+
+// Variables used for password generation. 
+var numbers = "0123456789";
+var special = "!@#$%^&*<>";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvqxyz";
+var FinalPw ="";
+var password = generatePassword();
+
+
+
+if (UserUpper===true){
+  FinalPw =  FinalPw + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+};
+
+if (UserLower===true){
+  FinalPw =  FinalPw + "abcdefghijklmnopqrstuvqxyz"
+};
+
+if (UserNumbers===true){
+  FinalPw =  FinalPw + "0123456789"
+};
+
+if (UserSpecial===true){
+  FinalPw =  FinalPw + "!@#$%^&*<>"
+};
+
+var password = "";
+
+for (i = 0; i < length; i++){
+  var password = password + Array.from(FinalPw)[(Math.floor(Math.random() * FinalPw.length))];
+};
+
+
+var passwordText = document.querySelector("#password");
+
+passwordText.value = password;
+
+
+
+
